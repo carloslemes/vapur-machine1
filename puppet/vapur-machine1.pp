4e3dfd5ccb826b7da79d81d904e5b27134ddfbbb
Marcos Brigante <marcos.brigante@gmail.com>
2013-06-08 15:36:43 -0300

    Creating the apache2 vshost using apache2's class. Fixed dependencies of modules. Passenger was not notifying the httpd service.

--- puppet/vapur-machine1.pp
+++ puppet/vapur-machine1.pp
@@ -28,8 +28,12 @@ exec { 'apt-get update':
 include gcc
 
 # Declare Apache
-class {'apache':
-	purge_vdir => false,
+#class {'apache':
+#	purge_vdir 	=> false,
+#}
+
+file { "/etc/apache2/sites-enabled/000-default":
+	ensure	=> absent,
 }
 
 # Declare Passenger, it does need parameters because I'm
@@ -41,6 +45,21 @@ class {'passenger':
   gem_path               => '/opt/ruby/lib/ruby/gems/1.9.1/gems',
   gem_binary_path        => '/opt/ruby/bin',
   mod_passenger_location => '/opt/ruby/lib/ruby/gems/1.9.1/gems/passenger-3.0.12/ext/apache2/mod_passenger.so',
+	notify								 => Service['httpd'],
+}
+
+apache::vhost { 'default':
+    priority        => '000',
+    vhost_name      => '*',
+    port            => '80',
+    docroot         => '/var/www/public',
+    serveradmin     => 'thisisatestmachine@example.com',
+}
+
+file { "/var/www":
+	ensure 	=> link,
+	target 	=> '/vagrant_data',
+	notify	=> Service['httpd'],
 }
 
 # Passenger looks for ruby on /usr/bin directory (see template passenger-load.erb),
